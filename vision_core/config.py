df33cc21675fd9a5769cfa63afcc60abaaef0442
Краснослободцев Сергей <solodeveloper.ai@gmail.com>
2026-01-07 12:11:45 +0300

    feat(config): добавить подробные docstring и привести структуру TableDetectorConfig, TablePreprocessorConfig и VisionCoreConfig к единому стилю

--- vision_core/config.py
+++ vision_core/config.py
@@ -1,13 +1,22 @@
 from dataclasses import dataclass, field
+from typing import Optional
 
 
 @dataclass
 class TableDetectorConfig:
-    """Конфигурация детектора таблиц"""
+    """Конфигурация детектора таблиц
+    - min_table_area: Минимальная площадь таблицы
+    - approx_poly_eps: Порог аппроксимации многоугольника
+    - mode_merge_cells: Режим объединения ячеек поддерживает:
+    > - all - объединяет строки и столбцы
+    > - cols - объединяет только столбцы
+    > - rows - объединяет только строки
+    > - None - не объединяет ячейки
+    """
 
     min_table_area: int = 10000
     approx_poly_eps: float = 0.02
-    mode_merge_cells: str = "cols"
+    mode_merge_cells: Optional[str] = "cols"
 
 
 @dataclass
@@ -27,44 +36,48 @@ class TableCellDetectorConfig:
 
 @dataclass
 class TablePreprocessorConfig:
-    """Конфигурация препроцессора таблиц"""
+    """Конфигурация препроцессора таблиц
+    - gaussian_blur_kernel: Размер ядра для гауссова размытия
+    - horizontal_length_ratio: Минимальная длина горизонтальных линий относительно высоты изображения
+    - vertical_length_ratio: Минимальная длина вертикальных линий относительно ширины изображения
+    - min_table_width_ratio: Минимальная ширина таблицы относительно ширины изображения
+    - min_table_height_ratio: Минимальная высота таблицы относительно ширины таблицы
+    """
 
     gaussian_blur_kernel: int = 1
 
-    horisontal_kernel_max: int = 50  # 50
-    horizontal_morph_kernel_size: int = 3
-    horizontal_min_length: int = 10
-    horizontal_min_intersections: int = 1
+    # Пороги для детекции линий
+    horizontal_length_ratio: float = 0.03
+    vertical_length_ratio: float = 0.006
 
-    vertical_morph_kernel_size: int = 3
-    vertical_kernel_max: int = 80
-    vertical_min_length: int = 120  # 120
-    vertical_min_intersections: int = 2
+    # Пороги для валидации таблиц
+    min_table_width_ratio: float = 0.5  # минимум 50% ширины изображения
+    min_table_height_ratio: float = 0.05  # минимум 5% от ширины таблицы
 
 
 @dataclass
 class PaddleOcrConfig:
     """Конфигурация для PaddleOCR"""
 
-    text_recognition_model_name = "cyrillic_PP-OCRv5_mobile_rec"
-    text_recognition_model_dir = "../models/cyrillic_PP-OCRv5_mobile_rec"
-    text_detection_model_name = "PP-OCRv5_server_det"
-    text_detection_model_dir = "../models/PP-OCRv5_server_det"
-    use_doc_orientation_classify = False
-    use_doc_unwarping = False
-    use_textline_orientation = False
-    device = "cpu"
+    text_recognition_model_name: str = "cyrillic_PP-OCRv5_mobile_rec"
+    text_recognition_model_dir: str = "../models/cyrillic_PP-OCRv5_mobile_rec"
+    text_detection_model_name: str = "PP-OCRv5_server_det"
+    text_detection_model_dir: str = "../models/PP-OCRv5_server_det"
+    use_doc_orientation_classify: bool = False
+    use_doc_unwarping: bool = False
+    use_textline_orientation: bool = False
+    device: str = "cpu"
 
 
 @dataclass
 class VisionCoreConfig:
     """Общая конфигурация для распознавания таблиц"""
 
-    detector: TableDetectorConfig = field(default_factory=TableDetectorConfig)
+    table_detector: TableDetectorConfig = field(default_factory=TableDetectorConfig)
     cell_detector: TableCellDetectorConfig = field(
         default_factory=TableCellDetectorConfig
     )
-    preprocessor: TablePreprocessorConfig = field(
+    table_preprocessor: TablePreprocessorConfig = field(
         default_factory=TablePreprocessorConfig
     )
 
@@ -74,10 +87,12 @@ class VisionCoreConfig:
     def from_dict(cls, config_dict: dict) -> "VisionCoreConfig":
         """Создание из словаря (для загрузки из JSON/YAML)"""
         return cls(
-            detector=TableDetectorConfig(**config_dict.get("detector", {})),
+            table_detector=TableDetectorConfig(**config_dict.get("table_detector", {})),
             cell_detector=TableCellDetectorConfig(
                 **config_dict.get("cell_detector", {})
             ),
-            preprocessor=TablePreprocessorConfig(**config_dict.get("preprocessor", {})),
+            table_preprocessor=TablePreprocessorConfig(
+                **config_dict.get("table_preprocessor", {})
+            ),
             paddleocr=PaddleOcrConfig(**config_dict.get("paddleocr", {})),
         )
