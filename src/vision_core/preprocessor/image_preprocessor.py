f2797cbf3a6aed9368040463e1286e232069263d
Сергей Краснослободцев <solodeveloper.ai@gmail.com>
2026-05-06 14:35:42 +0300

    refactor(image_preprocessor): обновляет документацию и заменяет метод _unsharp_mask на image_utils.unsharp_mask

--- src/vision_core/preprocessor/image_preprocessor.py
+++ src/vision_core/preprocessor/image_preprocessor.py
@@ -1,9 +1,16 @@
+"""Модуль для препроцессинга изображений с адаптивной обработкой.
+Этот модуль включает в себя класс ImagePreprocessor, который применяет нечеткую маску
+к изображению для улучшения его качества перед дальнейшей обработкой. Параметры маски
+настраиваются через конфигурацию ImagePreprocessorConfig, что позволяет адаптировать
+препроцессинг к различным типам изображений и условиям съемки.
+"""
+
 from __future__ import annotations
 
-import cv2
 import numpy as np
 
 from vision_core.config import ImagePreprocessorConfig
+from vision_core.utils import image_utils
 
 
 class ImagePreprocessor:
@@ -18,11 +25,11 @@ class ImagePreprocessor:
         self._debug = debug_image
 
     def process(self, image: np.ndarray, *, page_number: int = 0) -> np.ndarray:
-        """Анализирует и улучшает изображение."""
+        """Применяет нечеткую маску к изображению для улучшения его качества."""
         if self._debug:
             self._debug.on_debug_image(src_image=image, stage="1_original", prefix="page", page_number=page_number)
 
-        result = self._unsharp_mask(
+        result = image_utils.unsharp_mask(
             image,
             kernel_size=(self.cfg.kernel, self.cfg.kernel),
             sigma=self.cfg.sigma,
@@ -33,8 +40,3 @@ class ImagePreprocessor:
             self._debug.on_debug_image(src_image=result, stage="2_preprocessed", prefix="page", page_number=page_number)
 
         return result
-
-    def _unsharp_mask(self, image: np.ndarray, kernel_size=(5, 5), sigma=1.5, amount=1.0) -> np.ndarray:
-        blurred = cv2.GaussianBlur(image, kernel_size, sigma)
-        sharpened = cv2.addWeighted(image, 1 + amount, blurred, -amount, 0)
-        return sharpened
