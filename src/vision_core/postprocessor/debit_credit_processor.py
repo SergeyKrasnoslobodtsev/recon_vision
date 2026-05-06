065c5b9ac74149565321795cf33ebe0c71ab35ac
Сергей Краснослободцев <solodeveloper.ai@gmail.com>
2026-05-06 14:38:23 +0300

    refactor(DebitCreditProcessor): удаляет неиспользуемые импорты и закомментированный код для улучшения читаемости

--- src/vision_core/postprocessor/debit_credit_processor.py
+++ src/vision_core/postprocessor/debit_credit_processor.py
@@ -6,9 +6,6 @@ from loguru import logger
 
 from vision_core.entities.page import Page
 
-# from vision_core.entities.table import Table
-# from vision_core.utils.currency import parse_currency
-
 
 class DebitCreditProcessor:
     """Нормализует денежные значения в дебет/кредит ячейках.
@@ -28,16 +25,3 @@ class DebitCreditProcessor:
                 if not table.dc_cols:
                     continue
                 logger.debug(f"Колонки дебет/кредит таблицы {table.id}: {table.dc_cols}")
-                # self._normalize(table)
-
-    # def _normalize(self, table: Table) -> None:
-    #     header_row = table.get_dc_header_row()
-    #     for cell in table.cells:
-    #         if cell.col not in table.dc_cols:
-    #             continue
-    #         if cell.row <= header_row:
-    #             continue
-    #         try:
-    #             cell.value = f"{parse_currency(cell.value):.2f}"
-    #         except ValueError:
-    #             logger.warning(f"Не удалось распарсить {cell.value} в таблице {table.id} row={cell.row} col={cell.col}")
