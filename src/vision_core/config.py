8cc1114bfe4c23e5e723af926ee2602bad683585
Краснослободцев Сергей <solodeveloper.ai@gmail.com>
2026-07-17 10:55:25 +0300

    изменение окружения

--- src/vision_core/config.py
+++ src/vision_core/config.py
@@ -149,7 +149,7 @@ class PaddleOcrConfig:
     use_doc_orientation_classify: bool = False
     use_doc_unwarping: bool = False
     use_textline_orientation: bool = False
-    device: str = "gpu"
+    device: str = "cpu"
 
 
 @dataclass
